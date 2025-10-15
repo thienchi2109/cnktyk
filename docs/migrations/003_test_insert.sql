c64ec791e0bda01452019bb6e81aef90ba232fe4
thienchiedu <thienchi2109@gmail.com>
2025-10-15 12:05:04 +0700

    docs: Add corrected test SQL for Migration 003

---
+++ docs/migrations/003_test_insert.sql
@@ -0,0 +1,47 @@
+-- Test SQL for Migration 003: Extended Activity Fields
+-- This script inserts a test activity with all the new extended fields
+
+INSERT INTO "GhiNhanHoatDong" (
+  "MaNhanVien",
+  "TenHoatDong",
+  "SoTinChiQuyDoi",
+  "NguoiNhap",
+  "TrangThaiDuyet",
+  "HinhThucCapNhatKienThucYKhoa",
+  "ChiTietVaiTro",
+  "DonViToChuc",
+  "NgayBatDau",
+  "NgayKetThuc",
+  "SoTiet",
+  "BangChungSoGiayChungNhan"
+) VALUES (
+  (SELECT "MaNhanVien" FROM "NhanVien" LIMIT 1),
+  'Hội thảo Y học lâm sàng 2024',
+  5.5,
+  (SELECT "MaTaiKhoan" FROM "TaiKhoan" LIMIT 1),
+  'ChoDuyet',
+  'Hội thảo khoa học',
+  'Báo cáo viên',
+  'Bệnh viện Đa khoa Cần Thơ',
+  '2024-03-15',
+  '2024-03-17',
+  12,
+  'GCN-2024-001234'
+);
+
+-- Verify the insert
+SELECT 
+  "TenHoatDong",
+  "HinhThucCapNhatKienThucYKhoa",
+  "ChiTietVaiTro",
+  "DonViToChuc",
+  "NgayBatDau",
+  "NgayKetThuc",
+  "SoTiet",
+  "SoTinChiQuyDoi",
+  "BangChungSoGiayChungNhan",
+  "TrangThaiDuyet"
+FROM "GhiNhanHoatDong"
+WHERE "TenHoatDong" LIKE '%Hội thảo%'
+ORDER BY "CreatedAt" DESC
+LIMIT 1;
